b6a9477a15c3301974a3c3a8d4ab243062e7356d
pragyachik <pragya@tri3d.in>
2020-07-15 03:21:50 +0530

    fixing some changes, testing contract calls

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,13 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.6.6;
 
 contract Election {
     string public candidateName;
 
-    function Election () public {
+    constructor () public {
         candidateName = "Candidate 1";
     }
 
-    function setCandidate (string _name) public {
+    function setCandidate (string memory _name) public {
         candidateName = _name;
     }
 }
